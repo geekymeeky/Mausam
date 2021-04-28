60023cd0788767bed0e5005ab27639834f0823e6
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-04-28 12:38:00 +0530

    loading screen

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
