56a9b0f36ceba7dd6b5c935f9cd3b25b383fcc4a
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-05-01 11:30:26 +0530

    Added animation to loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mausam/services/weather_model.dart';
+import 'package:mausam/screens/weather_view.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,10 +12,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
+    getWeatherData();
+  }
+
+  void getWeatherData() async {
+    WeatherModel model = WeatherModel();
+    var weatherData = await model.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return WeatherView(
+        locationWeather: weatherData,
+      );
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: SpinKitDoubleBounce(
+        size: 100.0,
+        color: Colors.blueAccent,
+      ),
+    );
   }
 }
