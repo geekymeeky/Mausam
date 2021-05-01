a57f4aedcbf6e54c61f367842e40a55aa2f2361d
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-05-01 11:28:55 +0530

    Created backend to fetch current location weather

--- lib/services/weather_model.dart
+++ lib/services/weather_model.dart
@@ -1,7 +1,9 @@
 import 'package:mausam/services/location.dart';
 import 'package:mausam/services/networking.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/material.dart';
 
-const API_KEY = '4a79eaddd3628432d53934843f3414c5';
+const API_KEY = 'SECRET_API_KEY';
 const API_URI = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
@@ -12,7 +14,41 @@ class WeatherModel {
     NetworkHandler networkHelper = NetworkHandler(Uri.parse(
         '$API_URI?lat=${location.latitude}&lon=${location.longitude}&appid=$API_KEY&units=metric'));
 
-    var weatherData = await networkHelper.getData();
-    return weatherData;
+    var _weatherData = await networkHelper.getData();
+    return _weatherData;
+  }
+
+  IconData getWeatherIcon(dynamic weatherData) {
+    var id = weatherData['weather'][0]['id'];
+    if (id < 300) {
+      return FontAwesomeIcons.cloudShowersHeavy;
+    } else if (id < 400) {
+      return FontAwesomeIcons.cloudRain;
+    } else if (id < 600) {
+      return FontAwesomeIcons.cloudShowersHeavy;
+    } else if (id < 700) {
+      return FontAwesomeIcons.snowflake;
+    } else if (id < 800) {
+      if (id == 721) {
+        return FontAwesomeIcons.cloudSun;
+      }
+      return FontAwesomeIcons.smog;
+    } else if (id == 800) {
+      return FontAwesomeIcons.sun;
+    } else {
+      return FontAwesomeIcons.cloudSun;
+    }
+  }
+
+  Color getColor(int id) {
+    if (id < 600) {
+      return Colors.white70;
+    } else if (id < 700) {
+      return Colors.white;
+    } else if (id < 800) {
+      return Colors.white;
+    } else {
+      return Colors.orangeAccent;
+    }
   }
 }
